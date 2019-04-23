8df5226c2c6f8c2340b12bf0524a642faadd0c23
nbgdeki <radic.dejan.nbg@gmail.com>
2019-04-23 18:50:42 +0200

    stuck

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,15 +6,14 @@ let start = 1;
 let limit = 50;
 let url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${apiKey}&start=${start}&limit=${limit}&convert=USD`;
 
-setInterval(() => {}, 60000);
-
+// Fetching and outputing data
 fetch(proxyurl + url)
   .then(res => res.json())
   .then(data => {
     document.querySelector('.loader').style.display = 'none';
     console.log(data.data);
     let output = '';
-
+    setInterval(() => {}, 10000);
     data.data.forEach(doc => {
       let num =
         doc.quote.USD.percent_change_24h > 0 ? 'text-success' : 'text-danger';
@@ -24,7 +23,7 @@ fetch(proxyurl + url)
           <td>${doc.name}</td>
           <td>${doc.symbol}</td>
           <td>$ ${doc.quote.USD.price}</td>
-          <td class="${num} bold">${doc.quote.USD.percent_change_24h} %</td>
+          <td class="${num}">${doc.quote.USD.percent_change_24h} %</td>
           <td>
             <div class="input-group input-group-sm">
               <input type="number" class="form-control mb-1" />
@@ -33,9 +32,23 @@ fetch(proxyurl + url)
               </button>
             </div>
           </td>
+          <td class="myCoin"></td>
         </tr>
       `;
 
+      // Sta sam ovde uradio...
+      document.addEventListener('click', e => {
+        if (e.target.className.includes('btn')) {
+          let amount = document.querySelectorAll('.myCoin');
+          amount.forEach(number => {
+            number.innerHTML =
+              e.target.previousSibling.previousSibling.value *
+              doc.quote.USD.price;
+          });
+        }
+
+        e.preventDefault();
+      });
       document.getElementById('table-js').innerHTML = output;
     });
   })
