f5c4f6a7d422cb3982140934431eeb913dc9da25
nbgdeki <radic.dejan.nbg@gmail.com>
2019-04-24 11:53:07 +0200

    refresh on every 60 seconds

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,26 +1,26 @@
 import '../styles/index.scss';
 
-const proxyurl = 'https://cors-anywhere.herokuapp.com/';
-let apiKey = '6958609d-7fcc-44ef-8996-71ea12a520e9';
-let start = 1;
-let limit = 50;
-let url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${apiKey}&start=${start}&limit=${limit}&convert=USD`;
+const proxyurl = 'https://cors-anywhere.herokuapp.com/',
+  apiKey = '6958609d-7fcc-44ef-8996-71ea12a520e9',
+  start = 1,
+  limit = 50,
+  url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${apiKey}&start=${start}&limit=${limit}&convert=USD`;
 
-// Fetching and outputing data
-fetch(proxyurl + url)
-  .then(res => res.json())
-  .then(data => {
-    document.querySelector('.loader').style.display = 'none';
-    // console.log(data.data);
-    let output = '';
+const getData = () => {
+  // Fetching and outputing data
+  fetch(proxyurl + url)
+    .then(res => res.json())
+    .then(data => {
+      document.querySelector('.loader').style.display = 'none';
+      let output = '';
 
-    data.data.forEach(doc => {
-      let num =
-        doc.quote.USD.percent_change_24h > 0 ? 'text-success' : 'text-danger';
+      data.data.forEach(doc => {
+        let num =
+          doc.quote.USD.percent_change_24h > 0 ? 'text-success' : 'text-danger';
 
-      output += `
+        output += `
         <tr>
-          <td>${doc.name}</td>
+          <td><a class="text-primary" href="#">${doc.name}</a></td>
           <td>${doc.symbol}</td>
           <td>$ ${doc.quote.USD.price}</td>
           <td class="crypto-value" style="display:none;">${
@@ -39,66 +39,67 @@ fetch(proxyurl + url)
         </tr>
       `;
 
-      document.getElementById('table-js').innerHTML = output;
-    });
-
-    //Bind event handler
-    let buttons = document
-      .getElementById('table-js')
-      .getElementsByClassName('btn');
-
-    Array.from(buttons).forEach(button => {
-      button.addEventListener('click', e => {
-        const selectedRow = e.target.closest('tr');
-        let amount = selectedRow.querySelector('.myCoin');
-
-        // Parse String to number
-        const cryptoValue = parseFloat(
-          selectedRow.querySelector('.crypto-value').textContent
-        );
-        const myValue = parseFloat(selectedRow.querySelector('input').value);
-
-        amount.innerHTML = cryptoValue * myValue;
-
-        // if (isNaN(myValue) || myValue <= 0) {
-        //   amount.style.color = 'red';
-        //   amount.innerHTML = 'Please enter a valid number';
-        //   selectedRow.querySelector('input').style.border = '1px solid red';
-        // } else {
-        //   amount.innerHTML = cryptoValue * myValue;
-        //   selectedRow.querySelector('input').style.border = '1px solid #ced4da';
-        //   amount.style.color = 'black';
-        // }
+        document.getElementById('table-js').innerHTML = output;
       });
-    });
 
-    let inputs = document
-      .getElementById('table-js')
-      .getElementsByClassName('form-control');
+      //Counting crypto amount you own
+      let buttons = document
+        .getElementById('table-js')
+        .getElementsByClassName('btn');
 
-    Array.from(inputs).forEach(input => {
-      input.addEventListener('keyup', e => {
-        const selectedInput = e.target.closest('input');
-        console.log(selectedInput);
-        let buttons = document
-          .getElementById('table-js')
-          .getElementsByClassName('btn');
-        const selectedRow = e.target.closest('tr');
-        let amount = selectedRow.querySelector('.myCoin');
+      Array.from(buttons).forEach(button => {
+        button.addEventListener('click', e => {
+          const selectedRow = e.target.closest('tr');
+          let amount = selectedRow.querySelector('.myCoin');
 
-        const button = e.target.parentElement.querySelector('button');
+          // Parse String to number
+          const cryptoValue = parseFloat(
+            selectedRow.querySelector('.crypto-value').textContent
+          );
+          const myValue = parseFloat(selectedRow.querySelector('input').value);
 
-        if (selectedInput.value) {
-          button.removeAttribute('disabled');
-        } else {
-          button.setAttribute('disabled', true);
-          amount.innerHTML = '';
-        }
-
-        if (e.keyCode === 13) {
-          button.click();
-        }
+          amount.innerHTML = cryptoValue * myValue;
+        });
       });
-    });
-  })
-  .catch(err => console.log(err));
+
+      // Enabling buttons and input values submited on ENTER
+      let inputs = document
+        .getElementById('table-js')
+        .getElementsByClassName('form-control');
+
+      Array.from(inputs).forEach(input => {
+        input.addEventListener('keyup', e => {
+          const selectedInput = e.target.closest('input');
+          console.log(selectedInput);
+          let buttons = document
+            .getElementById('table-js')
+            .getElementsByClassName('btn');
+          const selectedRow = e.target.closest('tr');
+          let amount = selectedRow.querySelector('.myCoin');
+
+          const button = e.target.parentElement.querySelector('button');
+
+          if (selectedInput.value) {
+            button.removeAttribute('disabled');
+          } else {
+            button.setAttribute('disabled', true);
+            // Clearing value of your coin after clearing input
+            amount.innerHTML = '';
+          }
+
+          if (e.keyCode === 13) {
+            button.click();
+          }
+        });
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+getData();
+
+// Set refresh interval
+setInterval(() => {
+  getData();
+  console.log('reload!');
+}, 60000);
